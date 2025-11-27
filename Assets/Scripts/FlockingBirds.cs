93af498eaffb5e291403add8ef5d5a454301e704
MammadAlinia <mohammadalinia82@gmail.com>
2025-11-28 02:34:16 +0330

    fix native array bug

--- Assets/Scripts/FlockingBirds.cs
+++ Assets/Scripts/FlockingBirds.cs
@@ -1,21 +1,20 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using Unity.Collections;
+using Unity.Jobs;
+using Unity.Mathematics;
 using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.Serialization;
 using Random = UnityEngine.Random;
 
-public struct BoidData
-{
-    public Vector3 Position;
-    public Vector3 Velocity;
-}
 
 public class FlockingBirds : MonoBehaviour
 {
-    public Transform birdPrefab;
-    [Range(1, 1000)] public int birdCount;
+    public Mesh birdMesh;
+    [SerializeField] private Material birdMaterial;
+    [Range(1, 100000)] public int birdCount;
     [Range(0.0001f, 10f)] public float speed;
     [Range(5, 100f)] public float scale = 15f;
 
@@ -28,152 +27,76 @@ public class FlockingBirds : MonoBehaviour
     [Header("Range")] [Range(0.01f, 10f)] public float separationRange = 2f;
     [Range(0.01f, 10f)] public float visibleRange = 5f;
 
-    private Transform[] transforms;
 
-    private BoidData[] boidsData;
+
+    private NativeArray<Boid> boids;
+    private NativeArray<Boid> boidsOut;
+    public NativeArray<Matrix4x4> visualData;
 
 
     private void Start()
     {
-        transforms = new Transform[birdCount];
-        boidsData = new BoidData[birdCount];
-        Application.targetFrameRate = 60;
 
-        for (int i = 0; i < transforms.Length; i++)
+        visualData = new NativeArray<Matrix4x4>(birdCount, Allocator.Persistent);
+        boids = new NativeArray<Boid>(birdCount, Allocator.Persistent);
+        boidsOut = new NativeArray<Boid>(birdCount, Allocator.Persistent);
+
+        for (int i = 0; i < boids.Length; i++)
         {
-            boidsData[i].Position = Random.insideUnitCircle * 5;
-            boidsData[i].Velocity = new Vector2(Random.value, Random.value).normalized;
-
-
-            transforms[i] = Instantiate(birdPrefab, transform);
-            transforms[i].localScale = Vector3.one + Vector3.up / 2;
-            transforms[i].up = boidsData[i].Velocity;
+            boids[i] = new Boid()
+            {
+                Position = new float3(Random.value, Random.value, 0),
+                Velocity = new float3(Random.value, Random.value, 0)
+            };
         }
+
+
+        Camera.main.orthographicSize = scale;
+        cameraS = Camera.main.OrthographicBounds().size * 0.45f;
+        xRange = cameraS.x;
+        yRange = cameraS.y;
+    }
+
+    float xRange;
+    float yRange;
+    private Vector3 cameraS;
+
+    private void OnDisable()
+    {
+        boids.Dispose();
+        boidsOut.Dispose();
+        visualData.Dispose();
     }
 
     private void Update()
     {
-        Camera.main.orthographicSize = scale;
-        var cameraS = Camera.main.OrthographicBounds().size * 0.45f;
-
-        float xRange = cameraS.x;
-        float yRange = cameraS.y;
-
-        for (int i = 0; i < boidsData.Length; i++)
-
-        {
-            var boid = boidsData[i];
-
-            var transformObj = transforms[i];
-
-
-            var velocity = boidsData[i].Velocity;
-
-
-            var birdPosition = boid.Position;
-
-
-            // separation
-
-            var closeBoidIndices = new List<int>();
-            var inRangeBoidIndices = new List<int>();
-
-            var separationVector = Vector3.zero;
-            var avgVisibleVelocity = Vector3.zero;
-            var avgCenterPosition = Vector3.zero;
-
-            for (int bi = 0; bi < boidsData.Length; bi++) // optimize using spatial hashing or other search methods
-            {
-                if (i == bi) continue; // don't count the current boid  
-
-                var cBoid = boidsData[bi];
-                var distance = Vector3.Distance(birdPosition, cBoid.Position);
-
-                //separation rules
-                if (distance <= separationRange)
-                {
-                    closeBoidIndices.Add(bi);
-                    separationVector += (boid.Position - cBoid.Position);
-                }
-
-                // alignment
-                if (distance <= visibleRange)
-                {
-                    inRangeBoidIndices.Add(bi);
-                    avgVisibleVelocity += boidsData[bi].Velocity;
-
-                    // Cohesion
-                    avgCenterPosition += boidsData[bi].Position;
-                }
-            }
-
-            if (closeBoidIndices.Count > 0)
-            {
-                // separationVector /= closeBoidIndices.Count;
-                separationVector *= (separationStrength);
-            }
-
-            if (inRangeBoidIndices.Count > 0)
-            {
-                avgVisibleVelocity /= inRangeBoidIndices.Count;
-                avgVisibleVelocity = (avgVisibleVelocity - boid.Velocity) * alignmentStrength;
-
-                avgCenterPosition /= inRangeBoidIndices.Count;
-                avgCenterPosition = (avgCenterPosition - boid.Position) * cohesionStrength;
-            }
-
-
-            // screen edges
-            if (birdPosition.y < -yRange && velocity.y < 0)
-            {
-                velocity = Vector2.Reflect(velocity, Vector3.down);
-                birdPosition = new Vector3(birdPosition.x, -yRange, birdPosition.z);
-            }
-
-            if (birdPosition.y > yRange && velocity.y > 0)
-            {
-                velocity = Vector2.Reflect(velocity, Vector3.up);
-                birdPosition = new Vector3(birdPosition.x, yRange, birdPosition.z);
-            }
-
-            if (birdPosition.x < -xRange)
-            {
-                velocity = Vector2.Reflect(velocity, Vector3.left);
-                birdPosition = new Vector3(-xRange, birdPosition.y, birdPosition.z);
-            }
-
-            if (birdPosition.x > xRange)
-            {
-                velocity = Vector2.Reflect(velocity, Vector3.right);
-                birdPosition = new Vector3(xRange, birdPosition.y, birdPosition.z);
-            }
-
-            boid.Velocity = velocity;
-
-            var finalVelocity = (boid.Velocity.normalized + separationVector + avgVisibleVelocity + avgCenterPosition);
-
-
-            // final velocity
-            boid.Position = birdPosition + new Vector3(finalVelocity.x, finalVelocity.y, 0) * (speed * Time.deltaTime);
-            boid.Velocity = finalVelocity;
-            boidsData[i] = boid;
-
-            // visual
-            transformObj.position = boid.Position;
-            transformObj.up = boid.Velocity.normalized;
-        }
+        //  FlockingSingleThread();
+        FlockingJobs();
     }
-}
 
-public static class CameraExtensions
-{
-    public static Bounds OrthographicBounds(this Camera camera)
+
+    private void FlockingJobs()
     {
-        float screenAspect = (float)Screen.width / (float)Screen.height;
-        float cameraHeight = camera.orthographicSize * 2;
-        Bounds bounds = new Bounds(
-            camera.transform.position,
-            new Vector3(cameraHeight * screenAspect, cameraHeight, 0));
-        return bounds;
+        var job = new FlockingJob()
+        {
+            AvoidanceRange = separationRange,
+            AvoidanceForce = separationStrength,
+            AlignmentForce = alignmentStrength,
+            AlignmentRange = visibleRange,
+            CohesionForce = cohesionStrength,
+            BoidsDataIn = boids,
+            BoidsDataOut = boidsOut,
+            deltaTime = Time.deltaTime,
+            Speed = speed,
+            VisualData = visualData,
+            xRange = xRange,
+            yRange = yRange
+        };
+
+        var handle = job.Schedule(boids.Length, 550);
+        handle.Complete();
+        (boids, boidsOut) = (boidsOut, boids);
+
+        Graphics.DrawMeshInstanced(birdMesh, 0, birdMaterial, visualData.ToList());
     }
 }
--- Assets/Scripts/FlockingJob.cs
+++ Assets/Scripts/FlockingJob.cs
@@ -1,10 +1,97 @@
-﻿using Unity.Jobs;
+﻿using Unity.Burst;
+using Unity.Collections;
+using Unity.Jobs;
+using Unity.Mathematics;
+using UnityEngine;
 
-public struct FlockingJob : IJob
+[BurstCompile]
+public struct Boid
 {
-        
-    public void Execute()
+    public float3 Position;
+    public float3 Velocity;
+
+    public Boid(float3 position, float3 velocity)
     {
-            
+        Position = position;
+        Velocity = velocity;
+    }
+}
+
+[BurstCompile]
+public struct FlockingJob : IJobParallelFor
+{
+    [ReadOnly] public NativeArray<Boid> BoidsDataIn;
+    [WriteOnly] public NativeArray<Boid> BoidsDataOut;
+    public NativeArray<Matrix4x4> VisualData;
+
+    public float AvoidanceRange;
+    public float AvoidanceForce;
+
+    public float AlignmentRange;
+    public float AlignmentForce;
+
+    public float CohesionForce;
+    public float deltaTime;
+    public float Speed;
+    public float xRange;
+    public float yRange;
+
+
+    public void Execute(int i)
+    {
+        var closeBoidsCount = 0;
+        var inRangeBoidsCount = 0;
+        var avoidanceVector = float3.zero;
+        var avgVelocityVector = float3.zero;
+        var avgPosition = float3.zero;
+
+        var currentBoid = BoidsDataIn[i];
+        var position = currentBoid.Position;
+        var velocity = currentBoid.Velocity;
+
+
+        for (int j = 0; j < BoidsDataIn.Length; j++)
+        {
+            if (j == i) continue;
+            var otherBoid = BoidsDataIn[j];
+            var distance = math.distance(otherBoid.Position, position);
+
+            if (distance <= AvoidanceRange) // avoidance
+            {
+                closeBoidsCount++;
+                avoidanceVector += (position - otherBoid.Position);
+            }
+
+            if (distance <= AlignmentRange)
+            {
+                avgVelocityVector += otherBoid.Velocity;
+                avgPosition += otherBoid.Position;
+                inRangeBoidsCount++;
+            }
+        }
+
+        avoidanceVector = closeBoidsCount > 0 ? avoidanceVector * AvoidanceForce : 0;
+        avgVelocityVector = inRangeBoidsCount > 0
+            ? ((avgVelocityVector / inRangeBoidsCount) - velocity) * AlignmentForce
+            : 0;
+        avgPosition = inRangeBoidsCount > 0
+            ? ((avgPosition / inRangeBoidsCount) - position) * CohesionForce
+            : 0;
+        // edges
+        if (position.y < -yRange && velocity.y < 0) velocity.y = -velocity.y;
+        if (position.y > yRange && velocity.y > 0) velocity.y = -velocity.y;
+        if (position.x < -xRange && velocity.x < 0) velocity.x = -velocity.x;
+        if (position.x > xRange && velocity.x > 0) velocity.x = -velocity.x;
+
+        var finalVel = velocity + avoidanceVector + avgVelocityVector + avgPosition;
+        finalVel = math.normalize(finalVel) * Speed;
+
+        currentBoid.Velocity = finalVel;
+        currentBoid.Position += finalVel * deltaTime;
+        BoidsDataOut[i] = currentBoid;
+        // visual
+        VisualData[i] = Matrix4x4.TRS(currentBoid.Position,
+            quaternion.LookRotation(new float3(0f, 0f, 1f), math.normalize(currentBoid.Velocity)),
+            new float3(.5f, 1f, 1f));
     }
 }
