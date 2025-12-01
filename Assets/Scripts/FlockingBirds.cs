eaf776330ac6eb8a3317f25e4d7b139b5cfb17e8
MammadAlinia <mohammadalinia82@gmail.com>
2025-12-01 03:53:24 +0330

    add spatial hashing from 5t to 18t for 60 frame

--- Assets/Scripts/FlockingBirds.cs
+++ Assets/Scripts/FlockingBirds.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using Flocking;
+using Unity.Burst;
 using Unity.Collections;
 using Unity.Jobs;
 using Unity.Mathematics;
@@ -16,7 +17,7 @@ public class FlockingBirds : MonoBehaviour
 {
     public Mesh birdMesh;
     [SerializeField] private Material birdMaterial;
-    [Range(1, 100000)] public int birdCount;
+    [Range(1, 100000)] public int boidCount;
     [Range(0.0001f, 50f)] public float speed;
     [Range(5, 100f)] public float scale = 15f;
 
@@ -26,23 +27,25 @@ public class FlockingBirds : MonoBehaviour
     [Range(0.0f, 1f)] public float alignmentStrength = 0.5f;
     [Range(0.0f, 1f)] public float cohesionStrength = 0.5f;
 
-    [Header("Range")] [Range(0.01f, 10f)] public float separationRange = 2f;
-    [Range(0.01f, 10f)] public float visibleRange = 5f;
+    [Header("Range")] [Range(0.01f, 10f)] public float avoidanceRange = 2f;
+    [Range(0.01f, 10f)] public float alignmentRange = 5f;
     [Range(0.01f, 10f)] public float turnFactor = 5f;
 
 
     public NativeArray<Boid> boids;
     private NativeArray<Boid> boidsOut;
     public NativeArray<Matrix4x4> visualData;
-
+    private NativeParallelMultiHashMap<int, int> spatialHash;
     [Header("Grid")] public float cellSize = 2f;
 
 
     private void Start()
     {
         UpdateSpatialHash();
-        visualData = new NativeArray<Matrix4x4>(birdCount, Allocator.Persistent);
-        boids = new NativeArray<Boid>(birdCount, Allocator.Persistent);
+        visualData = new NativeArray<Matrix4x4>(boidCount, Allocator.Persistent);
+        boids = new NativeArray<Boid>(boidCount, Allocator.Persistent);
+        boidsOut = new NativeArray<Boid>(boidCount, Allocator.Persistent);
+        spatialHash = new NativeParallelMultiHashMap<int, int>(boidCount * 2, Allocator.Persistent);
 
 
         for (int i = 0; i < boids.Length; i++)
@@ -68,9 +71,10 @@ public class FlockingBirds : MonoBehaviour
 
     private void OnDisable()
     {
-        boids.Dispose();
-        boidsOut.Dispose();
-        visualData.Dispose();
+        if (boids.IsCreated) boids.Dispose();
+        if (boidsOut.IsCreated) boidsOut.Dispose();
+        if (spatialHash.IsCreated) spatialHash.Dispose();
+        if (visualData.IsCreated) visualData.Dispose();
     }
 
     private void Update()
@@ -86,12 +90,21 @@ public class FlockingBirds : MonoBehaviour
 
     private void FlockingJobs()
     {
+        float cellSize = math.max(avoidanceRange, alignmentRange);
+        spatialHash.Clear();
+        var hashJob = new HashPositionsJob()
+        {
+            boids = boids,
+            spatialHash = spatialHash.AsParallelWriter(),
+            cellSize = cellSize
+        };
+        var hashHandle = hashJob.Schedule(boids.Length, 550);
         var job = new FlockingJob()
         {
-            AvoidanceRange = separationRange,
+            AvoidanceRange = avoidanceRange,
             AvoidanceForce = separationStrength,
             AlignmentForce = alignmentStrength,
-            AlignmentRange = visibleRange,
+            AlignmentRange = alignmentRange,
             TurnFactor = turnFactor,
             CohesionForce = cohesionStrength,
             BoidsDataIn = boids,
@@ -100,18 +113,51 @@ public class FlockingBirds : MonoBehaviour
             Speed = speed,
             VisualData = visualData,
             XRange = xRange,
-            YRange = yRange
+            YRange = yRange,
+            spatialHash = spatialHash,
+            CellSize = cellSize
         };
 
 
-        var handle = job.Schedule(boids.Length, 550);
+        var handle = job.Schedule(boids.Length, 550, hashHandle);
         handle.Complete();
         (boids, boidsOut) = (boidsOut, boids);
         Graphics.DrawMeshInstanced(birdMesh, 0, birdMaterial, visualData.ToList());
     }
 
+    [BurstCompile]
+    public struct HashPositionsJob : IJobParallelFor
+    {
+        [ReadOnly] public NativeArray<Boid> boids;
+        [WriteOnly] public NativeParallelMultiHashMap<int, int>.ParallelWriter spatialHash;
+        public float cellSize;
+
+        public void Execute(int index)
+        {
+            var gridPos = GetGridPosition(boids[index].Position, cellSize);
+            var hash = Hash(gridPos);
+            spatialHash.Add(hash, index);
+        }
+
+        private static int Hash(int3 gridPos)
+        {
+            unchecked
+            {
+                return gridPos.x * 73856093 ^ gridPos.y * 19349663 ^ gridPos.z * 83492791;
+            }
+        }
+
+        private static int3 GetGridPosition(float3 position, float size)
+        {
+            return new int3(
+                (int)math.floor(position.x / size),
+                (int)math.floor(position.y / size),
+                (int)math.floor(position.z / size)
+            );
+        }
+    }
+
     private void OnDrawGizmos()
     {
-    
     }
 }
--- Assets/Scripts/FlockingJob.cs
+++ Assets/Scripts/FlockingJob.cs
@@ -1,91 +1,148 @@
-using System;
+using Flocking;
 using Unity.Burst;
 using Unity.Collections;
 using Unity.Jobs;
 using Unity.Mathematics;
 using UnityEngine;
 
-namespace Flocking
+[BurstCompile]
+public struct FlockingJob : IJobParallelFor
 {
-    [BurstCompile]
-    public struct FlockingJob : IJobParallelFor
+    [ReadOnly] public NativeArray<Boid> BoidsDataIn;
+    [ReadOnly] public NativeParallelMultiHashMap<int, int> spatialHash;
+
+    [WriteOnly] public NativeArray<Boid> BoidsDataOut;
+    public NativeArray<Matrix4x4> VisualData;
+
+    public float CellSize;
+    public float TurnFactor;
+    public float AvoidanceRange;
+    public float AvoidanceForce;
+    public float AlignmentRange;
+    public float AlignmentForce;
+    public float CohesionForce;
+    public float DeltaTime;
+    public float Speed;
+    public float XRange;
+    public float YRange;
+    public float ZRange;
+
+    public void Execute(int i)
     {
-        [ReadOnly] public NativeArray<Boid> BoidsDataIn;
-        [WriteOnly] public NativeArray<Boid> BoidsDataOut;
-        public NativeArray<Matrix4x4> VisualData;
-        public float TurnFactor;
-        public float AvoidanceRange;
-        public float AvoidanceForce;
+        var currentBoid = BoidsDataIn[i];
+        var position = currentBoid.Position;
+        var velocity = currentBoid.Velocity;
 
-        public float AlignmentRange;
-        public float AlignmentForce;
+        // Flocking calculations
+        var closeBoidsCount = 0;
+        var inRangeBoidsCount = 0;
+        var avoidanceVector = float3.zero;
+        var avgVelocityVector = float3.zero;
+        var avgPosition = float3.zero;
 
-        public float CohesionForce;
-        public float DeltaTime;
-        public float Speed;
-        public float XRange;
-        public float YRange;
+        var gridPos = GetGridPosition(position, CellSize);
 
-
-        public void Execute(int i)
+        // Check 27 neighboring cells (3x3x3)
+        for (int x = -1; x <= 1; x++)
         {
-            var closeBoidsCount = 0;
-            var inRangeBoidsCount = 0;
-            var avoidanceVector = float3.zero;
-            var avgVelocityVector = float3.zero;
-            var avgPosition = float3.zero;
-
-            var currentBoid = BoidsDataIn[i];
-            var position = currentBoid.Position;
-            var velocity = currentBoid.Velocity;
-
-
-            for (int j = 0; j < BoidsDataIn.Length; j++)
+            for (int y = -1; y <= 1; y++)
             {
-                if (j == i) continue;
-                var otherBoid = BoidsDataIn[j];
-                var distance = math.distance(otherBoid.Position, position);
+                var neighborCell = gridPos + new int3(x, y, 0);
+                var hash = Hash(neighborCell);
 
-                if (distance <= AvoidanceRange) // avoidance
+                // Query all boids in this cell
+                if (spatialHash.TryGetFirstValue(hash, out int otherIndex, out var iterator))
                 {
-                    closeBoidsCount++;
-                    avoidanceVector += (position - otherBoid.Position);
-                }
+                    do
+                    {
+                        if (otherIndex == i) continue;
 
-                if (distance <= AlignmentRange)
-                {
-                    avgVelocityVector += otherBoid.Velocity;
-                    avgPosition += otherBoid.Position;
-                    inRangeBoidsCount++;
+                        var otherBoid = BoidsDataIn[otherIndex];
+                        var distance = math.distance(otherBoid.Position, position);
+
+                        // Avoidance
+                        if (distance <= AvoidanceRange && distance > 0.001f)
+                        {
+                            closeBoidsCount++;
+                            avoidanceVector += (position - otherBoid.Position) / distance;
+                        }
+
+                        // Alignment & Cohesion
+                        if (distance <= AlignmentRange)
+                        {
+                            avgVelocityVector += otherBoid.Velocity;
+                            avgPosition += otherBoid.Position;
+                            inRangeBoidsCount++;
+                        }
+                    } while (spatialHash.TryGetNextValue(out otherIndex, ref iterator));
                 }
             }
-
-            avoidanceVector = closeBoidsCount > 0 ? avoidanceVector * AvoidanceForce : 0;
-            avgVelocityVector = inRangeBoidsCount > 0
-                ? ((avgVelocityVector / inRangeBoidsCount) - velocity) * AlignmentForce
-                : 0;
-            avgPosition = inRangeBoidsCount > 0
-                ? ((avgPosition / inRangeBoidsCount) - position) * CohesionForce
-                : 0;
-            // edges
-            if (position.y < -YRange && velocity.y <= 0) velocity.y += TurnFactor;
-            if (position.y > YRange && velocity.y >= 0) velocity.y -= TurnFactor;
-            if (position.x < -XRange && velocity.x <= 0) velocity.x += TurnFactor;
-            if (position.x > XRange && velocity.x >= 0) velocity.x -= TurnFactor;
-
-            var finalVel = velocity + avoidanceVector + avgVelocityVector + avgPosition;
-            finalVel = math.normalize(finalVel) * Speed;
-
-            currentBoid.Velocity = finalVel;
-            currentBoid.Position += finalVel * DeltaTime;
-
-
-            BoidsDataOut[i] = currentBoid;
-
-            // visual
-            VisualData[i] = Matrix4x4.TRS(currentBoid.Position,
-                quaternion.LookRotation(new float3(0f, 0f, 1f), math.normalize(currentBoid.Velocity)),
-                new float3(.5f, 1f, 1f));
         }
+
+        // Apply flocking forces
+        if (closeBoidsCount > 0)
+        {
+            avoidanceVector = math.normalize(avoidanceVector) * AvoidanceForce;
+        }
+
+        if (inRangeBoidsCount > 0)
+        {
+            avgVelocityVector = ((avgVelocityVector / inRangeBoidsCount) - velocity) * AlignmentForce;
+            avgPosition = ((avgPosition / inRangeBoidsCount) - position) * CohesionForce;
+        }
+
+        // Boundary constraints
+        if (position.y < -YRange && velocity.y <= 0) velocity.y += TurnFactor;
+        if (position.y > YRange && velocity.y >= 0) velocity.y -= TurnFactor;
+        if (position.x < -XRange && velocity.x <= 0) velocity.x += TurnFactor;
+        if (position.x > XRange && velocity.x >= 0) velocity.x -= TurnFactor;
+        if (position.z < -ZRange && velocity.z <= 0) velocity.z += TurnFactor;
+        if (position.z > ZRange && velocity.z >= 0) velocity.z -= TurnFactor;
+
+        // Calculate final velocity
+        var finalVel = velocity + avoidanceVector + avgVelocityVector + avgPosition;
+        var speed = math.length(finalVel);
+
+        if (speed > 0.001f)
+        {
+            finalVel = math.normalize(finalVel) * Speed;
+        }
+        else
+        {
+            finalVel = new float3(0, 0, 1) * Speed; // Default direction
+        }
+
+        currentBoid.Velocity = finalVel;
+        currentBoid.Position += finalVel * DeltaTime;
+
+        BoidsDataOut[i] = currentBoid;
+
+        // Visual transform
+        var direction = math.length(currentBoid.Velocity) > 0.001f
+            ? math.normalize(currentBoid.Velocity)
+            : new float3(0, 1, 0);
+
+        VisualData[i] = Matrix4x4.TRS(
+            currentBoid.Position,
+            quaternion.LookRotation(new float3(0f, 0f, 1f), direction),
+            new float3(0.5f, 1f, 1f)
+        );
+    }
+
+    private static int Hash(int3 gridPos)
+    {
+        unchecked
+        {
+            return gridPos.x * 73856093 ^ gridPos.y * 19349663 ^ gridPos.z * 83492791;
+        }
+    }
+
+    private static int3 GetGridPosition(float3 position, float size)
+    {
+        return new int3(
+            (int)math.floor(position.x / size),
+            (int)math.floor(position.y / size),
+            (int)math.floor(position.z / size)
+        );
     }
 }
